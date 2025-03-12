85c0a9d2c082e313f29d3b6d243fc43a777f58cf
mpoe philip <philip.mpoe@student.moringaschool.com>
2025-03-12 15:52:22 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+// Challenge 1: Multiply Two Numbers
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// Challenge 2: Generate a Random Integer
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Challenge 3: Calculate the Remainder
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+
+// Challenge 4: Find the Maximum Number
+const max = Math.max(10, 15, 20, 5);
